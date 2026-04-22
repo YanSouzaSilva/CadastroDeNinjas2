ea7922f4676a0f0427c1308d67e027f7b3e9d312
Yan Souza Silva <yanafonso@gmail.com>
2026-04-22 12:34:12 -0300

    Criado a Controller

--- src/main/java/dev/java10x/CadastroDeNinjas2/Ninja/Controller.java
+++ src/main/java/dev/java10x/CadastroDeNinjas2/Ninja/Controller.java
@@ -1,12 +1,15 @@
 package dev.java10x.CadastroDeNinjas2.Ninja;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/ninjas")
 public class Controller {
 
-    @GetMapping("/teste")
-    public String teste() {
-        return "Funcionando!";
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "Essa é minha primeira mensagem nessa rota.";
     }
+
 }
