f91856719296fd149df60c5957646c0b03d4cdb3
Yan Souza Silva <yanafonso@gmail.com>
2026-04-22 18:55:06 -0300

    Cria uma entidade no banco de dados a partir do NinjaModel

---
+++ src/main/java/dev/java10x/CadastroDeNinjas2/Ninja/NinjaModel.java
@@ -0,0 +1,25 @@
+package dev.java10x.CadastroDeNinjas2.Ninja;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_cadastro")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private int idade;
+
+    private String email;
+}
