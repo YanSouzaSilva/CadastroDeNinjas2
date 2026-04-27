a3947cc45e9bfc21a272ebfa1e1be5fb5d30fb4c
Yan Souza Silva <yanafonso@gmail.com>
2026-04-27 10:57:56 -0300

    feat: Criado rotas do MissoesController com mock

--- src/main/java/dev/java10x/CadastroDeNinjas2/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas2/Missoes/MissoesController.java
@@ -1,5 +1,38 @@
 package dev.java10x.CadastroDeNinjas2.Missoes;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/missoes")
 public class MissoesController {
+
+    @GetMapping("/boasvindas")
+    public String boasVindasMissoes(){
+        return "Boas vindas a seção das missões";
+    }
+
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "Missão Criada";
+    }
+
+    @GetMapping("/listar")
+    public String mostrarMissoes(){
+        return "Mostrando todas missões";
+    }
+
+    @GetMapping("/listarID")
+    public String mostrarMissaoID(){
+        return "Mostrando missão pelo ID";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissaoID(){
+        return "Alterando missao pelo ID";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletarMissaoID(){
+        return "Mostrando todas missões";
+    }
 
 }
