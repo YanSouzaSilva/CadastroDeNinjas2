fbe1284b48fcc91452471ede78d253072cd7916a
Yan Afonso <yanafonso@gmail.com>
2026-04-30 12:19:21 -0300

    feat: Cria corpo do NinjaDTO

--- src/main/java/dev/java10x/CadastroDeNinjas2/Ninja/NinjaDTO.java
+++ src/main/java/dev/java10x/CadastroDeNinjas2/Ninja/NinjaDTO.java
@@ -1,4 +1,23 @@
 package dev.java10x.CadastroDeNinjas2.Ninja;
+import dev.java10x.CadastroDeNinjas2.Missoes.MissoesModel;
+import jakarta.persistence.*;
 
 public class NinjaDTO {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private int idade;
+
+    @Column(name = "img_url")
+    private String imgUrl;
+
+    @Column(unique = true)
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "missao_id") //chave estrangeira
+    private MissoesModel missao;
 }
